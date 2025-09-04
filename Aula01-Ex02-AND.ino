74ac9b48ed17e9d096d191b417ff2d7b413eadd7
Viviane Park <viviflorp@gmail.com>
2025-09-04 11:41:30 -0300

    Update Aula01-Ex02-AND.ino

--- Aula01-Ex02-AND.ino
+++ Aula01-Ex02-AND.ino
@@ -1,4 +1,7 @@
-// OR
+// Crie um programa que leia dois valores em sequência no Monitor
+// Serial. Os valores possíveis são ‘0’ ou ‘1’. Em posse desses valores,
+// acenda o LED interno da placa seguindo o padrão das portas lógicas
+// AND, OR e XOR.
 
 void setup() {
   Serial.begin(115200);
@@ -7,15 +10,10 @@ void setup() {
 
 void loop() {
   if (Serial.available() == 2) {
-    char v1 = Serial.read();
-    char v2 = Serial.read();
+    int v1 = Serial.read() - 48;  // ascii '0' = 48
+    int v2 = Serial.read() - 48;
 
-    if ((v1 == '0' || v1 == '1') && (v2 == '0' || v2 == '1')) {
-      if (v1 == '0' && v2 == '0') {
-        digitalWrite(LED_BUILTIN, LOW);
-      } else {
-        digitalWrite(LED_BUILTIN, HIGH);
-      }
-    }
+    if ((v1 == 0 || v1 == 1) && (v2 == 0 || v2 == 1))
+      digitalWrite(LED_BUILTIN, v1 && v2);
   }
 }
