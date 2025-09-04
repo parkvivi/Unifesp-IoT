e6ac2b6c390b1af6a673e0392184d895b0f1731d
Viviane Park <viviflorp@gmail.com>
2025-09-04 11:40:10 -0300

    Update Aula01-Ex01.ino

--- Aula01-Ex01.ino
+++ Aula01-Ex01.ino
@@ -1,3 +1,7 @@
+// Crie um programa que leia um valor pelo Monitor Serial. Se o valor for
+// igual a ‘a’, o LED interno da placa deve acender. Se o valor for igual a
+// ‘b’, o LED deve apagar.
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
