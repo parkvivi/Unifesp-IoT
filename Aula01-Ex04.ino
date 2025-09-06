2f1bc5bbc926e270e8dd88c864d70a49fc082239
Viviane Park <viviflorp@gmail.com>
2025-09-06 15:22:05 -0300

    Update Aula01-Ex04.ino

--- Aula01-Ex04.ino
+++ Aula01-Ex04.ino
@@ -11,27 +11,30 @@ const int LED_2 = 13;
 
 const int t = 2000;
 
+int EstadoAtual = Idle;
+
 void setup() {
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
 }
 
 void loop() {
-  for (int EstadoAtual = 0; EstadoAtual < 3; ++EstadoAtual) {
-    switch(EstadoAtual) {
-      case Idle:
-        digitalWrite(LED_1, LOW);
-        digitalWrite(LED_2, LOW);
-        break;
-      case Scanning:
-        digitalWrite(LED_1, HIGH);
-        digitalWrite(LED_2, LOW);
-        break;
-      case Executing:
-        digitalWrite(LED_1, HIGH);
-        digitalWrite(LED_2, HIGH);
-        break;
-    }
-    delay(t);
+  switch(EstadoAtual) {
+    case Idle:
+      digitalWrite(LED_1, LOW);
+      digitalWrite(LED_2, LOW);
+      EstadoAtual = Scanning;
+      break;
+    case Scanning:
+      digitalWrite(LED_1, HIGH);
+      digitalWrite(LED_2, LOW);
+      EstadoAtual = Executing;
+      break;
+    case Executing:
+      digitalWrite(LED_1, HIGH);
+      digitalWrite(LED_2, HIGH);
+      EstadoAtual = Idle;
+      break;
   }
+  delay(t);
 }
